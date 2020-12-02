450f5c9356f8acfc99ef93cfa5a57deaa6af956c
TreeSome <youn@tree-some.dev>
2020-12-02 22:04:13 +0900

    init express dev server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-const str:string = 'Hello World';
-console.log(str);
+import Express from 'express';
+import cors from 'cors';
+
+import http from 'http';
+
+const IS_DEV = process.env.NODE_ENV === 'developement';
+const app = Express();
+
+app.use(cors());
+
+app.all('/', (req: Express.Request, res: Express.Response) => {
+	res.send(`OK. Your requested [${req.method}]. And you are [${req.header('user-agent')}].\n`);
+});
+
+if ( IS_DEV ) {
+	const httpServer = http.createServer(app);
+	httpServer.listen(IS_DEV ? 3080 : 80, () => {
+		console.log('Listen HTTP Server', IS_DEV ? 3080 : 80);
+	});
+}
