3d5c1e3c4608947c35fcf57c504a6f7c82ad3879
TreeSome <youn@tree-some.dev>
2020-12-03 18:03:46 +0900

    seo

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,21 @@
 const path = require('path');
+const PrerenderSpaPlugin = require('prerender-spa-plugin');
+
+const plugins = [];
+if ( process.argv[2] === "build" ) {
+	plugins.push(new PrerenderSpaPlugin({
+		staticDir: path.resolve(__dirname, 'web-dist'),
+		indexPath: path.resolve(__dirname, 'web-dist', 'index.html'),
+		captureAfterTime: 5000,
+		routes: [
+			"/",
+		],
+	}));
+}
 
 module.exports = {
 	configureWebpack: {
+		plugins,
 		entry: [
 			path.join(__dirname, "web-src", "index.js"),
 		],
